3e37b4dc9ded691b4a128fe0ae122fe61d8ac256
Nae <120901516+NaesSnippit@users.noreply.github.com>
2023-07-26 11:01:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,66 +1,64 @@
-// need the const fs variable here
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-// need inquirer variable here
-const inquirer = require("inquirer");
-
-// need a markdown js file here
-const generateMarkdown = require("./utils/generateMarkdown");
-
-
-// License function and  if/else section here 
-function getLicense(value) {
-    if (value === "GNU AGPLv3") {
-        return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
-    } else if (value === "GNU GPLv3") {
-        return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-    } else if (value === "GNU LGPLv3") {
-        return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
-    } else if (value === "Apache 2.0") {
-        return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-    } else if (value === "Boost Software 1.0") {
-        return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-    } else if (value === "MIT") {
-        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-    } else {
-        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-    }
-}
-
-
-function validateInput(value) {
-    if (value != "") {
-        return true;
-    } else {
-        return "Please answer the question with some kind on input.";
-    }
-}
-
-
+// Internal modules
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+// TODO: Create an array of questions for user input
 const questions = [
-    // Question for the Title
     {
-        type: "input",
-        name: "title",
+        type: 'input',
+        message: "What is your GitHub username? (No @ needed)",
+        name: 'username',
+        default: 'danielthomas129',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub username is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+        default: 'readme-generator',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub repo is required for a badge.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
         message: "What is the title of your project?",
-        validate: validateInput,
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
     },
-    // Question for the project Description
     {
-        type: "input",
-        name: "description",
-        message: "Please enter a description of your project.",
-        validate: validateInput,
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
     },
-
-    // Table of Contents, andling this in the markdown.js
-
-    // Question for Installation
     {
-        type: "input",
-        name: "installation",
-        message: "Please enter an explanation how to install the software, or commands for the program.",
-        validate: validateInput,
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
     },
 
     // Question for Usage
